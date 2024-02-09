c9f54fb948026b75053082660695e0e78f7493b4
evanbacon <bacon@expo.io>
2024-02-09 14:49:39 -0600

    add comments to explain my metro config

--- example/metro.config.js
+++ example/metro.config.js
@@ -6,9 +6,14 @@ const workspaceRoot = path.resolve(__dirname, "..");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.unstable_enableSymlinks = true;
-config.resolver.blockList = [/node_modules\/expo-quick-actions\/example\//];
+// Tell metro to include files outside of this directory, to include the parent project.
 config.watchFolders = [projectRoot, workspaceRoot];
+
+config.resolver.blockList = [
+  // Prevent the resolver from creating recursive loops.
+  /node_modules\/expo-quick-actions\/example\//,
+];
+// When a node module is resolved, Metro should check both the parent project and the current node_modules directory.
 config.resolver.nodeModulesPath = [
   path.resolve(projectRoot, "node_modules"),
   path.resolve(workspaceRoot, "node_modules"),
@@ -16,11 +21,11 @@ config.resolver.nodeModulesPath = [
 
 for (const m of [
   "expo-modules-core",
-  // "react-native/Libraries/EventEmitter/RCTDeviceEventEmitter",
   "react",
   "react-native",
   "expo-quick-actions",
 ]) {
+  // When a module fails to resolve, Metro should look for it here. This helps reduce the change of duplicate modules being included and creating scoped memory issues.
   config.resolver.extraNodeModules[m] = path.resolve(
     require.resolve(m + "/package.json"),
     ".."
