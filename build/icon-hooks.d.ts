320172db20c9fa11992630968a24aaa970808ae7
evanbacon <bacon@expo.io>
2024-12-29 18:36:07 -0600

    wip

--- build/icon-hooks.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export default function DynamicIconProvider({ children, }: {
-    children: React.ReactNode;
-}): React.JSX.Element;
-export declare function useSelectedIconSource(): any | null;
-export declare function useAppIcon(): (string | ((iconName: string | null) => void) | null)[];
-//# sourceMappingURL=icon-hooks.d.ts.map
