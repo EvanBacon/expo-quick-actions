f069c679a5bfc24f92d009a49bc354ea2da1c79e
evanbacon <bacon@expo.io>
2025-05-02 13:40:29 -0500

    revert package name change

--- example/android/app/src/main/java/com/bacon/example/MainActivity.kt
+++ example/android/app/src/main/java/com/bacon/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bacon.demo
+package com.bacon.example
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- example/android/app/src/main/java/com/bacon/example/MainApplication.kt
+++ example/android/app/src/main/java/com/bacon/example/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.bacon.demo
+package com.bacon.example
 
 import android.app.Application
 import android.content.res.Configuration
