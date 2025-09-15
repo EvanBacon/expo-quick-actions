6d4c3dc48170fafa7538b0932aeebdec6fa7eb79
evanbacon <bacon@expo.io>
2025-09-15 11:14:14 -0700

    + expo-quick-actions@6.0.0

---
+++ build/index.d.ts
@@ -0,0 +1,22 @@
+import type { SFSymbol } from "sf-symbols-typescript";
+type AppleBuiltInIcons = "compose" | "play" | "pause" | "add" | "location" | "search" | "share" | "prohibit" | "contact" | "home" | "markLocation" | "favorite" | "love" | "cloud" | "invitation" | "confirmation" | "mail" | "message" | "date" | "time" | "capturePhoto" | "captureVideo" | "task" | "taskCompleted" | "alarm" | "bookmark" | "shuffle" | "audio" | "update";
+type AppleSymbolId = `symbol:${SFSymbol}`;
+type LocalAssetId = `asset:${string}`;
+export type Action = {
+    id: string;
+    title: string;
+    icon?: AppleBuiltInIcons | AppleSymbolId | LocalAssetId | (string & {}) | null;
+    /** iOS-only. Subtitle for the action. */
+    subtitle?: string | null;
+    /** Additional serial parameters for the action.  */
+    params?: Record<string, number | string | boolean | null | undefined> | null;
+};
+export declare const initial: Action | undefined;
+export declare const maxCount: number | undefined;
+export declare const setItems: <TAction extends Action = Action>(data?: TAction[]) => Promise<void>;
+export declare const isSupported: () => Promise<boolean>;
+export declare function addListener<TAction extends Action = Action>(listener: (action: TAction) => void): {
+    remove: () => void;
+};
+export {};
+//# sourceMappingURL=index.d.ts.map
